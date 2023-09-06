5111e06c0920f72dc9b524423bbcf89df3d92b81
MotacillaAlba <rowena.lily.lee@gmail.com>
2023-09-06 23:51:33 +0800

    🔨 : working on data analysis

--- trajectory_data_analysis/utils/levelx.py
+++ trajectory_data_analysis/utils/levelx.py
@@ -1,12 +1,10 @@
 import os
 import json
-from multiprocessing.dummy import Pool as ThreadPool
 import time
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import matplotlib.transforms as mtransforms
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -22,13 +20,19 @@ mpl.rcParams.update(
 )
 
 
-def merge_dictionary(dict1, dict2):
+column_name = {
+    "highD": {"x": "x", "y": "y", "id": "id"},
+    "inD": {"x": "xCenter", "y": "yCenter", "id": "recordingId"},
+}
+
+
+def _merge_dictionary(dict1, dict2):
     for key, value in dict2.items():
         if key not in dict1:
             dict1[key] = value
         else:
             if isinstance(value, dict):
-                merge_dictionary(dict1[key], value)
+                _merge_dictionary(dict1[key], value)
             else:
                 dict1[key] += value
     return dict1
@@ -51,7 +55,10 @@ def _get_plot_range(df: pd.DataFrame):
 def plot_map_and_trajectories(
     map_name, data_path, img_path, transform, type_order, configs
 ):
+    dataset = map_name.split("_")[0]
     map_img = mpimg.imread(os.path.join(img_path, map_name + ".png"))
+    if dataset != "highD":
+        map_img = np.flipud(map_img)
 
     file_id = configs[map_name]["trajectory_files"][0]
     trajectory_path = os.path.join(data_path, "%02d_tracks.csv" % file_id)
@@ -61,18 +68,24 @@ def plot_map_and_trajectories(
 
     trajectory_types = dict()
     for _, line in df_metadata.iterrows():
-        trajectory_types[int(line["id"])] = line["class"]
+        trajectory_types[int(line[column_name[dataset]["id"]])] = line["class"]
 
     locations = list()
     for chunk in df_trajectory:
         for _, line in chunk.iterrows():
-            locations.append([line["x"], line["y"], trajectory_types[int(line["id"])]])
+            locations.append(
+                [
+                    line[column_name[dataset]["x"]],
+                    line[column_name[dataset]["y"]],
+                    trajectory_types[int(line[column_name[dataset]["id"]])],
+                ]
+            )
 
     df = pd.DataFrame(locations, columns=["x", "y", "class"])
     x_min, x_max, y_min, y_max = _get_plot_range(df)
 
     fig, ax = plt.subplots()
-    fig.set_figwidth(12)
+    fig.set_figwidth(9)
     ax.set_xlim(x_min, x_max)
     ax.set_ylim(y_min, y_max)
     im = ax.imshow(map_img, aspect="equal")
@@ -93,12 +106,63 @@ def plot_map_and_trajectories(
     plt.show()
 
 
-def plot_speed_distribution(dataset, data_path, type_order, configs):
+def plot_class_proportion(dataset, data_path, type_order, configs):
+    df_proportion = pd.DataFrame(columns=["map", "file_id", "class", "proportion"])
+    for map_name, value in configs.items():
+        if not dataset in map_name:
+            continue
+
+        for file_id in value["trajectory_files"]:
+            cnts = dict()
+            trajectory_metadata_path = os.path.join(
+                data_path, "%02d_tracksMeta.csv" % file_id
+            )
+            df_metadata = pd.read_csv(trajectory_metadata_path)
+
+            for _, line in df_metadata.iterrows():
+                cnts[line["class"]] = cnts.get(line["class"], 0) + 1
+
+            sum_cnt = sum(cnts.values())
+            accumulated_proportion = dict()
+            for i, type_ in enumerate(type_order):
+                accumulated_proportion[type_] = (
+                    sum([cnts.get(type_, 0) for type_ in type_order[i:]]) / sum_cnt
+                )
+
+            for cnt_key, cnt_proportion in accumulated_proportion.items():
+                df_proportion.loc[len(df_proportion.index)] = [
+                    map_name,
+                    file_id,
+                    cnt_key,
+                    cnt_proportion,
+                ]
+
+    plot = sns.FacetGrid(
+        df_proportion,
+        col="map",
+        col_wrap=1,
+        sharex=False,
+        sharey=True,
+        xlim=(0, 60),
+        ylim=(0, 1.0),
+        height=1,
+        aspect=3,
+        hue="class",
+        palette="husl",
+        legend_out=True,
+        hue_order=type_order,
+    )
+    plot.map(sns.barplot, "file_id", "proportion")
+    plot.add_legend()
+    plt.show()
+
+
+def plot_mean_speed_distribution(dataset, data_path, type_order, configs):
+    speeds = list()
     for key, value in configs.items():
         if not dataset in key:
             continue
 
-        speeds = list()
         for file_id in value["trajectory_files"]:
             trajectory_metadata_path = os.path.join(
                 data_path, "%02d_tracksMeta.csv" % file_id
@@ -115,7 +179,8 @@ def plot_speed_distribution(dataset, data_path, type_order, configs):
         col="map",
         col_wrap=2,
         sharex=True,
-        sharey=False,
+        sharey=True,
+        height=1,
         aspect=1.5,
         hue="class",
         palette="husl",
@@ -124,6 +189,11 @@ def plot_speed_distribution(dataset, data_path, type_order, configs):
     plot.map(sns.kdeplot, "meanSpeed", fill=True, alpha=0.5)
     plot.add_legend()
     plt.show()
+
+
+def plot_angle_distribution(dataset, data_path, type, configs):
+
+
 
 
 def _count_lane_change(trajectory_file_path):
@@ -158,7 +228,7 @@ def plot_lane_change_distribution(dataset, data_path, type_order, configs):
         if map_key not in maps:
             maps[map_key] = lane_change_distribution
         else:
-            merge_dictionary(maps[map_key], lane_change_distribution)
+            _merge_dictionary(maps[map_key], lane_change_distribution)
 
     df = pd.DataFrame(columns=["map", "class", "numLaneChanges", "count"])
     for map_key, lane_change_dist in maps.items():
